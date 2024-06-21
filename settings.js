53b0d0c02947c9be457302bd03adbe5d9291d358
Nnalue Kingsley <164803861+Ebukannalue1132@users.noreply.github.com>
2024-06-21 10:07:31 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2349070810971"
-global.ownername = "🐛Tech God"
+global.ownernomer = "2349045746625"
+global.ownername = "🐛xavage"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '2349070810971'  //creator number
-global.ownername = '🐛Tech god' //owner name
+global.ownernumber = '2349045746625'  //creator number
+global.ownername = '🐛xavage' //owner name
 global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
